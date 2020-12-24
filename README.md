b7e642643c024bbef4e43a15af79312dad430250
brickmaker <zhaoxiaodong@zju.edu.cn>
2020-12-24 10:03:29 +0800

    docs: ✏️ update api reference

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
 # netv-lasso-selection
 
-lasso plugin for NetV.js(http://netv.zjuvag.org/)
+Lasso plugin for NetV.js(http://netv.zjuvag.org/)
 
+## API reference
 
+### `lasso = new Lasso(netv, configs)`
+
+Create lasso handler
+
+* `netv: NetV`: pass core NetV object
+* `configs: {}`: key-value pair configs
+    * `enable: bool`: default enable lasso or not
+
+### `lasso.dispose()`
+
+Dispose lasso handler, clean related DOM element
+
+### `lasso.enable()`
+
+Manually enable lasso selection.
+
+### `lasso.disable()`
+
+Manually disable lasso selection.
+
+### `lasso.closeDistance(distance: number)`
+
+Set lasso's closeDistance to control the tolerance.
+
+### `lasso.onSelected(callback: (nodes: Node[]) => {})`
+
+Set callback function, the callback can get selected nodes in NetV's Node type.
--- example/index.js
+++ example/index.js
@@ -1,25 +1,43 @@
-const svg = document.querySelector('#main')
-svg.setAttribute('width', 800)
-svg.setAttribute('height', 600)
-
-const items = [
-    { id: 0, x: 200, y: 10 },
-    { id: 1, x: 400, y: 300 },
-    { id: 2, x: 120, y: 500 },
-    { id: 3, x: 600, y: 400 },
+/**
+ * @author Xiaodong Zhao <zhaoxiaodong@zju.edu.cn>
+ * @description lasso operation
+ */
+const netv = new NetV({
+    container: document.getElementById('main'),
+    link: {
+        style: {
+            strokeWidth: 1
+        }
+    }
+})
+const data = NetV.Utils.transformGraphPosition(netv.loadDataset('miserables'), 500, 400, 300)
+const colorMap = [
+    { r: 166, g: 206, b: 227, a: 0.9 },
+    { r: 178, g: 223, b: 138, a: 0.9 },
+    { r: 31, g: 120, b: 180, a: 0.9 },
+    { r: 51, g: 160, b: 44, a: 0.9 },
+    { r: 251, g: 154, b: 153, a: 0.9 },
+    { r: 227, g: 26, b: 28, a: 0.9 },
+    { r: 253, g: 191, b: 111, a: 0.9 },
+    { r: 255, g: 127, b: 0, a: 0.9 },
+    { r: 202, g: 178, b: 214, a: 0.9 },
+    { r: 106, g: 61, b: 154, a: 0.9 },
+    { r: 255, g: 255, b: 153, a: 0.9 },
+    { r: 177, g: 89, b: 40, a: 0.9 }
 ]
+data.nodes.forEach((node) => {
+    const { r, g, b, a } = colorMap[node.group]
+    node.style = {
+        fill: { r: r / 255, g: g / 255, b: b / 255, a }
+    }
+})
+netv.data(data)
+netv.draw()
 
-for (const item of items) {
-    const circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle')
-    circle.setAttribute('cx', item.x)
-    circle.setAttribute('cy', item.y)
-    circle.setAttribute('fill', 'red')
-    circle.setAttribute('r', 10)
-    svg.appendChild(circle)
-}
+netv.on('pan', () => { })
+netv.on('zoom', () => { })
 
-const lasso = new Lasso(svg)
-lasso.data(items)
+const lasso = new Lasso(netv, { enable: true })
 lasso.onSelected((selectedItems) => {
     console.log(selectedItems)
 })
